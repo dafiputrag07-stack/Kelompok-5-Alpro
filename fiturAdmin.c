bcc3f40a123ee391282f7f310c874d96f7d7d221
Dzaudan <mdzaudanarsyala@student.upi.edu>
2025-12-17 18:11:18 +0700

    Update fiturAdmin.c

--- fiturAdmin.c
+++ fiturAdmin.c
@@ -8,11 +8,13 @@
 
 
 void tambahWarga() {
-    if (jumlahWarga >= 100) { printf("Database Penuh!\n"); return; }
-
+    if (jumlahWarga >= 100) { 
+        printf("Database Penuh!\n"); 
+        return; 
+    }
     Warga b = {0};
     printf("\n--- INPUT DATA WARGA BARU ---\n");
-    b.nik = inputAngka("Pendapatan (Rp): "); getchar();
+    printf("NIK: "); scanf("%s", b.nik); getchar();
     printf("Nama Lengkap: "); fgets(b.nama, 50, stdin); b.nama[strcspn(b.nama, "\n")] = 0;
     b.pendapatan = inputAngka("Pendapatan (Rp): ");
     b.tanggungan = (int)inputAngka("Jumlah Tanggungan: ");
@@ -28,40 +30,57 @@ void tambahWarga() {
     hitungSkor(jumlahWarga);
     jumlahWarga++;
     simpanFile();
-    printf(">>> Data berhasil disimpan! Tekan Enter..."); getchar();
+    printf(">>> Data berhasil disimpan! Tekan Enter...");
+    getchar();
 }
 
 void editWarga() {
+    char cariDataWarga[50];
+    int ditemukan = -1;
     printf("\n--- EDIT DATA WARGA ---\n");
-    cariData();
-        for (int i = 0; i < jumlahWarga; i++) {
-        printf("\nData: %s (Skor: %.1f)\n", db[i].nama, db[i].skorAkhir);
-        printf("--- Masukkan Data Baru ---\n");
-        printf("Nama Baru: "); fgets(db[i].nama, 50, stdin);
-        db[i].nama[strcspn(db[i].nama, "\n")] = 0;
-        db[i].pendapatan = inputAngka("Pendapatan Baru: ");
-        db[i].tanggungan = inputAngka("Tanggungan Baru: ");
-        db[i].kondisiRumah = inputAngka("Kondisi Rumah (1.Layak 2.Sedang 3.Rusak): ");
-        db[i].statusKerja = inputAngka("Status Kerja (1.Tetap 2.Kontrak 3.Tidak): ");
-        db[i].totalAset = inputAngka("Total Aset Baru (Rp): ");
-
-        hitungSkor(i);
-        simpanFile();
-        printf("[v] Data Update! Tekan Enter..."); getchar();
+    printf("Nama/NIK: ");
+    fgets(cariDataWarga, 50, stdin);
+    cariDataWarga[strcspn(cariDataWarga, "\n")] = 0;
+    for (int i = 0; i < jumlahWarga; i++) {
+        if (strstr(db[i].nama, cariDataWarga) != NULL ||strcmp(db[i].nik, cariDataWarga) == 0) { 
+            ditemukan = i; 
+            break;
+        }
     }
-}
+        if (ditemukan != -1) {
+            printf("\nData: %s (Skor: %.1f)\n", db[ditemukan].nama, db[ditemukan].skorAkhir);
+            printf("--- Masukkan Data Baru ---\n");
+            printf("Nama Baru: "); fgets(db[ditemukan].nama, 50, stdin);
+            db[ditemukan].nama[strcspn(db[ditemukan].nama, "\n")] = 0;
+            db[ditemukan].pendapatan = inputAngka("Pendapatan Baru: ");
+            db[ditemukan].tanggungan = inputAngka("Tanggungan Baru: ");
+            db[ditemukan].kondisiRumah = inputAngka("Kondisi Rumah (1.Layak 2.Sedang 3.Rusak): ");
+            db[ditemukan].statusKerja = inputAngka("Status Kerja (1.Tetap 2.Kontrak 3.Tidak): ");
+            db[ditemukan].totalAset = inputAngka("Total Aset Baru (Rp): ");
 
+            hitungSkor(ditemukan);
+            simpanFile();
+            printf("[v] Data Update! Tekan Enter...");
+            getchar();
+        } else 
+            printf("[!] Tidak Ditemukan.\n");
+            printf("Tekan Enter..."); 
+            getchar();
+    }
 
 void hapusWarga() {
-    char cariNIK[20];
+    char cariDataWarga[50];
     int ditemukan = -1;
-    printf("\n--- HAPUS DATA WARGA ---\nCari NIK: ");
-    scanf("%s", cariNIK); getchar();
-
+    printf("\n--- HAPUS DATA WARGA ---\n");
+    printf("Nama/NIK: ");
+    fgets(cariDataWarga, 50, stdin);
+    cariDataWarga[strcspn(cariDataWarga, "\n")] = 0;
     for (int i = 0; i < jumlahWarga; i++) {
-        if (strcmp(db[i].nik, cariNIK) == 0) { ditemukan = i; break; }
+        if (strstr(db[i].nama, cariDataWarga) != NULL ||strcmp(db[i].nik, cariDataWarga) == 0) { 
+            ditemukan = i; 
+            break; 
+        }
     }
-
     if (ditemukan != -1) {
         printf("Hapus %s (Gol: %d)? (y/n): ", db[ditemukan].nama, db[ditemukan].golongan);
         char yakin; scanf("%c", &yakin); getchar();
@@ -69,8 +88,10 @@ void hapusWarga() {
             for (int j = ditemukan; j < jumlahWarga - 1; j++) db[j] = db[j + 1];
             jumlahWarga--; simpanFile();
             printf("[v] Terhapus.\n");
-        } else printf("[x] Batal.\n");
-    } else printf("[!] Tidak Ditemukan.\n");
-
-    printf("Tekan Enter..."); getchar();
+        } else 
+            printf("[x] Batal.\n");
+    } else 
+        printf("[!] Tidak Ditemukan.\n");
+        printf("Tekan Enter..."); 
+        getchar();
 }
